c7252867d6734a638826b0bc444b9cc3954d7832
Cedric Poulet <cpoulet@student.42.fr>
2017-12-20 16:41:00 +0100

    histo + scatter

--- src/scatter_plot.py
+++ src/scatter_plot.py
@@ -5,16 +5,22 @@ import sys
 
 FILE = '../data/dataset_train.csv'
 HOGWARTS_HOUSE = {b'Gryffindor': 2, b'Hufflepuff': 3, b'Ravenclaw': 0, b'Slytherin': 1}
+HOUSE_CONV = np.vectorize(lambda x: HOGWARTS_HOUSE[x])
+
+EPOC_PATTERN = '%Y-%m-%d'
+EPOC_CONV = np.vectorize(lambda x: int(time.mktime(time.strptime(x.decode(),EPOC_PATTERN))))
 
 def histogram(f):
 
     data = np.genfromtxt(f, delimiter=',', names=True, dtype=None)
+    #data['Hogwarts_House']=HOUSE_CONV(data['Hogwarts_House'])
+    #data['Birthday']=EPOC_CONV(data['Birthday'])
     
     FIELD = data.dtype.names[6:]
     for f in FIELD:
-        rslt = [data[data['Hogwarts_House'] == h][f] for h in HOGWARTS_HOUSE]
-        rslt = [arr[~np.isnan(arr)] for arr in rslt]
-        plt.hist(rslt, bins=20, alpha=0.8, histtype='stepfilled')
+        r = data[f]
+        r.sort()
+        plt.scatter(r, np.arange(len(r)))
         plt.title(f)
         plt.show()
 
